3f2f4b14e7a8f2c0a43f2c0563f7d425acb8a31a
1shad <s.h.a.d@free.fr>
2017-08-16 16:41:56 +0200

    App.pm: utilisation des interfaces, update version et doc

--- lib/Pstreamer/App.pm
+++ lib/Pstreamer/App.pm
@@ -8,17 +8,16 @@ Pstreamer::App - Application de streaming vidéo
 
 =head1 VERSION
 
- Version 0.005
+ Version 0.006
 
 =cut
 
-our $VERSION = '0.005';
+our $VERSION = '0.006';
 
 use utf8;
 use feature 'say';
 use Try::Tiny;
-use Term::ANSIColor 'colored';
-use Scalar::Util qw(looks_like_number refaddr reftype);
+use Scalar::Util qw(refaddr reftype);
 use Pstreamer::Util::CloudFlare;
 use Pstreamer::Viewer;
 use Pstreamer::Host;
@@ -27,34 +26,40 @@ use Pstreamer::Config;
 use Moo;
 use MooX::Options description => 'perldoc Pstreamer::App pour les détails';
 
-option go => (
-    is => 'ro',
-    format => 's@',
-    default => sub { [] },
-    autosplit => ',',
-    doc => "Automate, ex: --go=2,'ma serie'",
+option ncurses => (
+    is          => 'rw',
+    negativable => 1,
+    doc         => "Active l'interface ncurses",
 );
 
 option fullscreen => (
-    is => 'ro',
-    short => 'fs',
+    is          => 'ro',
+    short       => 'fs',
     negativable => 1,
-    doc => 'Videos en plein ecran',
+    doc         => 'Videos en plein ecran',
+);
+
+option go => (
+    is        => 'ro',
+    format    => 's@',
+    default   => sub { [] },
+    autosplit => ',',
+    doc       => "Automate, ex: --go=2,'ma serie'",
 );
 
 option version => (
-    is => 'ro',
+    is    => 'ro',
     short => 'v',
-    doc => 'Affiche la version',
+    doc   => 'Affiche la version',
 );
 
-has [qw(config ua tx stash term viewer host site cf command)] => (
+has [qw(config ua tx viewer host site cf UI)] => (
     is => 'rw',
 );
 
 # LIFO
 has history => (
-    is => 'ro',
+    is      => 'ro',
     default => sub { [] },
 );
 
@@ -72,8 +77,11 @@ before run => sub { shift->_init };
 sub _init {
     my $self = shift;
 
+    $self->ncurses(0) if @{$self->go};
+
     my %options = (
         fullscreen => $self->fullscreen,
+        ncurses    => $self->ncurses,
     );
 
     defined $options{$_} or delete $options{$_} for keys %options;
@@ -84,118 +92,82 @@ sub _init {
     $self->site( Pstreamer::Site->new );
     $self->cf( Pstreamer::Util::CloudFlare->new );
     $self->ua( $self->config->ua );
-    $self->term( $self->config->term );
-    $self->command( [qw(:quit :h :s :p :m)] );
-    $self->term->addhistory( $_ ) for @{$self->command};
-    push @{$self->command}, qw(:q q :exit);
+    $self->UI( $self->config->ui );
 }
 
-# the program loop
 sub run {
     my $self = shift;
-    my ( @choices, @tmp, $line );
-    my $one_choice_str = '';
 
     if ( $self->version ) {
         say "\nThis is Pstreamer::App version $VERSION\n";
         exit;
     }
+    
+    # catch all warnings
+    $SIG{__WARN__} = sub { return 1; };
 
-    while ( 1 ) {
-        my $count = 0;
-
-        @choices = $self->site->get_sites unless $self->site->current;
-
-        # if only one host or stream url, it doesn't show the menu
-        if ( @choices == 1 and ! $self->_is_internal( $choices[0]->{url} ) ) {
-            # breaks recursion if any
-            if ( $one_choice_str =~ /$choices[0]->{url}/ ){
-                $line = ':p';
-                $one_choice_str = "";
-            } else {
-                $line = 0;
-                $one_choice_str .= $choices[0]->{url};
-            }
-        }
-        elsif ( @{$self->go} ) {
-            $line = shift @{$self->go};
-            say $self->stash and $self->stash(undef) if $self->stash;
-            if ( $line eq 'print') {
-                $self->_print_choices( @choices ) if $line eq 'print';
-                $line = shift @{$self->go} // ':q';
-            }
-        }
-        else { # get user input
-            $one_choice_str = ''; # assures that it is empty
-            $self->_print_choices( @choices );
-            if ( $self->stash ) {
-                say colored( $self->stash, 'red' );
-                $self->stash(undef);
-            }
-            $line = $self->term->readline(colored(">>> ", 'bold'));
-        }
-        
-        # proceed user input
-        next unless defined $line and $line ne "";
-
-        if ( $self->_is_command( $line ) ) {
-            @tmp = $self->_proceed_command( $line );
-        }
-        elsif ( looks_like_number($line) && $line < @choices ) {
-            @tmp = $self->_proceed_line( $choices[$line] );
-        }
-        else {
-            @tmp = $self->_proceed_search($line);
-        }
-        $self->stash( 'Aucun résultat') unless @tmp or $line eq ':h';
-        @choices = @tmp if @tmp;
-    }
+    $self->UI->init;
+    $self->UI->controller( $self );
+    $self->UI->site_list( [$self->site->get_sites] );
+    # start with the list of sites
+    $self->UI->list( [$self->site->get_sites] );
+    $self->UI->run;
 }
 
-# formats and prints the array of choices
-sub _print_choices {
-    my ( $self, @choices ) = @_;
-    my $count = 0;
+sub proceed {
+    my ( $self, $element ) = @_;
+    my $one_choice_str = '';
+    return unless $element;
     
-    @choices = grep {
-        defined
-        and refaddr($_)
-        and reftype($_) eq reftype {}
-        and $_->{name}
-    } @choices;
-
-    @choices = map {
-        join '', ($count>9?"":" ",colored($count++ ,'bold'),": ",$_->{name});
-    } @choices;
-
-    say join "\n", @choices if @choices;
+    my @list = $self->_proceed_line( $element );
+    
+    while ( @list == 1 and ! $self->_is_internal( $list[0]->{url} ) ) {
+        # breaks recursion if any
+        if ( $one_choice_str =~ /$list[0]->{url}/ ) {
+            @list = $self->UI->list;
+            last;
+        } else {
+            $one_choice_str .= $list[0]->{url};
+        }
+        @list = $self->_proceed_line( $list[0] );
+    }
+    
+    $self->UI->error('Aucun résultat') unless @list;
+    $self->UI->list( [@list] );
 }
 
-# processes the choosen command by the user
-sub _proceed_command {
-    my ( $self, $line ) = @_;
-    my ( $previous, @choices );
+sub proceed_previous {
+    my $self = shift;
+    my @list;
+
+    my $previous = $self->history;
+
+    unless ( $previous ) {
+        $self->UI->list( [$self->site->get_sites] );
+        $self->UI->site_name( undef );
+        $self->UI->menu_list( undef );
+        $self->site->current( undef );
+        return;
+    }
     
-    $line =~ s/\s*([^\s]*)\s*.*/$1/;
-    exit if $line =~ /^(:q|q|:quit|:exit)$/;
+    $self->tx( $self->_get( $previous ) );
+    @list = $self->site->get_results( $self->tx );
+    $self->UI->list( [@list] );
+}
 
-    if ( $line eq ":s" ) {
-        @choices = $self->site->get_sites;
-    }
-    elsif ( $line eq ":m" ) {
-        return undef unless $self->site->current;
-        @choices = @{$self->site->menu};
-    }
-    elsif ( $line eq ":p"){
-        $previous = $self->history;
-        return $self->site->current(undef) unless $previous;
-        $self->tx( $self->_get($previous) );
-        @choices = $self->site->get_results($self->tx);
-    } else {
-        $self->_help;
-    }
+# processes search text written by the user
+sub proceed_search {
+    my ( $self, $text ) = @_;
+    my @list;
+    return unless $self->site->current;
+    return unless $text and $text ne "";
 
-    return @choices;
+    $self->history( $self->tx->req->url ) if $self->tx;
+    $self->tx( $self->site->search($text) );
+    @list = $self->site->get_results($self->tx);
+    
+    $self->UI->error('Aucun résultat') unless @list;
+    $self->UI->list( [@list] );
 }
 
 # processes the selected line by the user
@@ -211,6 +183,8 @@ sub _proceed_line {
     for( $element ) {
         if( $_->{url} =~ /^PICO$/ ) { # site choice
             $self->site->current( $_->{name} );
+            $self->UI->site_name( $_->{name} );
+            $self->UI->menu_list( $self->site->menu );
             # it needs to follow redirects here
             $self->ua->max_redirects(5);
             $self->tx( $self->_get( $self->site->url ) );
@@ -236,14 +210,14 @@ sub _proceed_line {
             try {
                 $res = $self->_proceed_host( $_->{url} );
             } catch {
-                warn $_ ;
+                $self->UI->error( $_ );
             };
             return @{$res} if $res;
         }
     }
 
     try { @choices = $self->site->get_results($self->tx); }
-    catch { warn $_ ; };
+    catch { $self->UI->error( $_ ); };
     return @choices;
 }
 
@@ -261,25 +235,12 @@ sub _proceed_host {
         $res = $res; # ...
     }
     elsif ( !$res and defined $res ) { # = 0
-        $self->stash('Fichier introuvable');
+        $self->UI->error('Fichier introuvable');
     }
     elsif( !defined $res ) { # = undef
-        $self->stash("Ce lecteur n'est pas supporté");
+        $self->UI->error("Ce lecteur n'est pas supporté");
     }
     return $res;
-}
-
-# processes search text written by the user
-sub _proceed_search {
-    my ( $self, $line ) = @_;
-    my @choices;
-    return undef unless $self->site->current;
-
-    $self->history( $self->tx->req->url ) if $self->tx;
-    $self->tx( $self->site->search($line) );
-    @choices = $self->site->get_results($self->tx);
-
-    return @choices;
 }
 
 # get url with Mojo::UserAgent and bypass cloudflare IMUA if it is active
@@ -291,21 +252,10 @@ sub _get {
         $tx = $self->ua->get( $url );
         $tx = $self->cf->bypass if $self->cf->is_active( $tx );
     } catch {
-        warn ( $_ );
+        $self->UI->error( $_ );
     };
     
     return $tx;
-}
-
-# returns true or false if the param is a command or not
-sub _is_command {
-    my ( $self, $line ) = @_;
-    return undef unless $line;
-    
-    $line =~ s/\s*([^\s]*)\s*.*/$1/;
-    
-    my $commands = join '|', @{$self->command};
-    return  $line =~ /^($commands)$/;
 }
 
 # Returns true or false if link is internal to the site or not
@@ -324,28 +274,12 @@ sub _is_internal {
     return 1;
 }
 
-# prints help
-sub _help {
-    my $self = shift;
-    my @text;
-
-    push @text, "[Recherche]";
-    push @text, "taper un texte puis Entrée";
-    push @text, "\n[Commandes]";
-    push @text, ":s\t: Afficher les sites";
-    push @text, ":m\t: Afficher le menu du site";
-    push @text, ":p\t: Précedent";
-    push @text, ":q\t: Quitter le programme. alias: q, :quit, :exit\n";
-    say  join "\n", @text;
-    $self->term->readline('Appuyer sur Entrée pour continuer');
-}
-
 1;
 
 =head1 DESCRIPTION
 
 Pstreamer permet de visionner des films ou des series,  
-en streaming, depuis un terminal. Il fonctionne sous GNU/Linux.
+en streaming, depuis un terminal, sur un système de type Unix.
 
 Il se connecte à certains sites français. Permet de les parcourir  
 via les liens disponibles ou par son menu, et permet de faire des recherches.  
@@ -356,6 +290,13 @@ Les vidéos sont lues depuis les hébergeurs avec mpv.
 Pour lancer pstreamer, exécutez:
 
     $ pstreamer
+
+=head1 INTERFACES
+
+Il y a deux interfaces disponibles: 'text' et 'ncurses'.  
+L'interface text est utilisée par défault.
+
+=head2 TEXT
 
 Le programme affiche les liens avec des numéros en début de ligne.  
 Taper le numéro de la ligne puis entrée pour continuer ou alors  
@@ -368,6 +309,27 @@ Les commandes du prompt disponibles sont:
     :s afficher les sites
     :q quitter
     :h aide  
+
+=head2 NCURSES
+
+Le programme affiche les liens disponible. Selectionner le lien  
+en descandant ou en montant avec les flèches du clavier bas et haut,  
+ou avec les touches 'k' et 'j', puis valider avec la touche entrer ou  
+fleche droite ou 'l'.  
+Revenir en arrière avec les touches flèche gauche ou 'h'.  
+
+Voici la liste des racourcis:
+
+    'j', 'bas'    : descendre dans la liste
+    'k', 'haut'   : monter dans la liste
+    'h', 'gauche' : precedant
+    'l', 'droite' : suivant
+
+    's' : menu selection d'un site
+    'm' : menu du site (inactif si aucun site n'est selectionner)
+    '>' : recherche
+
+    Control-q : quitter
 
 =head1 OPTIONS
 
@@ -385,54 +347,81 @@ Lance mpv en plein écran. Désactivée par défault.
 =head2 --no-fullscreen
 
 Désactive le plein écran, si l'option est activée dans  
-fichier de configuration.
+le fichier de configuration.
+
+=head2 --ncurses
+
+Active l'interface ncurses
+
+=head2 --no-ncurses
+
+Désactive l'interface ncurses si l'option est activée  
+dans le fichier de configuration.
 
 =head2 --go
 
-pstreamer est capable d'automatiser les entrées.
+Cette option n'est disponible que avec l'interface text,  
+donc cette interface est automatiquement utilisé avec.
 
-Avec cette option, vous pouvez aller directement à la page  
-voulue, si vous connaissez d'avance les numéros et les textes  
-que vous auriez écrit en lancant le programme.  
+Elle permet d'automatiser les entrées.  
+Le programme etant un 'bot', elle est utile pour réaliser des  
+scripts ou des alias pour faire des bookmarks.  
+Vous trouverez un exemple de script à la fin de cette section.
 
-exemple 1: 
+Les commandes utilisées sont celles de l'interface text, plus  
+la commande 'print'.
 
-    $ pstreamer  --go=2,'ma série'
+Donc au lieu d'entrer dans le programme,  
+puis taper 2,entrer,'ma serie',entrer,0,entrer
 
-    permet de sélectionner le 3eme site, puis
-    de faire une recherche avec le texte 'ma série'.
-    pstreamer affiche les résultats, puis le prompt.
+Vous pouvez ecrire: 
 
-exemple 2:
+    $ pstreamer --go=2,'ma série',0
 
-    $ pstreamer --fs --go=2,'ma serie',2,1,0,0,:q
 
-    pstreamer lance automatiquement l'épisode avec 
-    mpv en plein ecran. Il quitte juste apres mpv.
+Si vous souhaitez juste afficher les résultats, pour vérifier  
+qu'un épisode est disponible, il faut utiliser la commande 'print'.
 
-exemple 3:
+exemple:
 
-    $ pstreamer --go=2,'ma serie',2,2,0,0,:p,1,0,:q
+    $ pstreamer --go=2,'ma serie',2,1,print
 
-    enchaine automatiquement les deux premiers épisodes
-    de 'ma serie' puis quitte.
 
-'print' permet d'afficher les résultats.
-
-Si vous exécutez:
-
-    $ pstreamer --go=2,'ma serie',:q
-
-le programme quitte mais n'affiche rien.
-
-Il faut alors entrer:
-
-    $ pstreamer --go=2,'ma serie',print
-
-pstreamer affiche les résultats puis quitte.
+le programme affiche les résultats puis quitte.  
+Si vous utilisez la commande ':q', le programme  
+n'affichera pas les résultats, donc il faut remplacer ':q'  
+par 'print'.
 
 Si il n'y a rien derrière print le programme quitte,  
 sinon il continue avec ce qui suit.
+
+Voici un petit script bash à lancer avec la crontab  
+qui permet d'alerter l'utilisateur qu'un episode est  
+disponible:
+
+    #!/bin/bash
+    export DISPLAY=:0.0         # requis pour notify-send
+    export PATH=$HOME/bin:$PATH # selon votre installation
+
+    # a omettre ou adapter selon votre installation de perl    
+    export PERL5LIB=$HOME/localperl/lib/perl5
+
+    # Comme je fais souvent un 'ls' dans mon repertoire home
+    # je ne peux pas le louper, et ca évite d'avoir plusieurs
+    # fois la nofitication.
+    if [ -f $HOME/MYSCRIPT.lock ]; then
+        exit 0;
+    fi
+
+    res=$(pstreamer --go=3,'game of',0,6,print|grep 'Episode 4'|wc -l)
+
+    if [ $res -gt 0 ]; then
+        notify-send "Episode dispo"
+        touch $HOME/MYSCRIPT.lock
+    fi
+
+C'est bien sur completement inutile mais si besoin,  
+a adapter selon votre recherche et votre installation.
 
 =head1 CONFIGURATION
 
@@ -440,7 +429,7 @@ Vous pouvez utiliser un fichier de configuration, si vous
 le souhaitez, pour paramétrer quelques propriétés ou options.
 
 Le format du fichier est libre, mais dépends d'un certain module.  
-Installez un des modules de la liste pour utiliser le format voulue.
+Installez un des modules de la liste pour utiliser le format voulu.
 
 La liste des formats:
 
@@ -521,6 +510,8 @@ L'emplacement du fichier est pour l'instant:
 
 =item Try::Tiny
 
+=item Curses::UI
+
 =back
 
 =head2 Modules perl recommandés:
@@ -568,7 +559,7 @@ Sinon, installez les dépendances, avec par exemple pour debian :
     libio-socket-ssl-perl libmoox-singleton-perl libmoox-options-perl \
     libmoox-configfromfile-perl libclass-inspector-perl libfile-homedir-perl \
     libtry-tiny-perl libdata-record-perl libregexp-common-perl \
-    libterm-readline-gnu-perl libconfig-tiny-perl
+    libterm-readline-gnu-perl libconfig-tiny-perl libcurses-ui-perl
 
 Et ensuite depuis le répertoire :
 
@@ -576,6 +567,11 @@ Et ensuite depuis le répertoire :
     $ make
     $ make test
     $ sudo make install
+
+NB:  
+Veuillez installer la librairie libncursesw5-dev, si vous  
+utilisez cpanm, avant d'installer le module Curses.  
+Sinon l'utf-8 ne sera pas pris en compte.
 
 =head1 MISES A JOUR
 
@@ -598,7 +594,7 @@ Si vous n'avez pas gardé le répertoire :
 
 =head1 INSTALLATION DE WWW::Mechanize::PhantomJS
 
-Déja, phantomjs n'est utilisé que pour l'hebergeur openload.  
+Phantomjs n'est utilisé que pour l'hebergeur openload.  
 Comme il n'y a pas de paquet pour l'installer il faut le faire avec cpanm.
 
 Si vous souhaitez l'installer, voilà la procédure :
