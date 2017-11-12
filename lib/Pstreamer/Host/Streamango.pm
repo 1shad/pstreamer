bb09eb337503edec728868d2f0e13842e702160f
1shad <s.h.a.d@free.fr>
2017-11-12 16:02:05 +0100

    Streamango.pm: update

--- lib/Pstreamer/Host/Streamango.pm
+++ lib/Pstreamer/Host/Streamango.pm
@@ -12,21 +12,53 @@ with 'Pstreamer::Role::UA';
 
 sub get_filename {
     my ( $self, $url ) = @_;
-    my ( $tx, $dom, $file );
+    my ( $tx, $dom, $file, $e, $c );
 
     $tx = $self->ua->get( $url );
     return 0 unless $tx->success;
     $dom = $tx->res->dom;
-    
-    ($file) = $dom =~ /{type:"video\/mp4",src:"([^"]+)",/;
-    if ($file) {
-        return $file = [{
-            url => 'https:'.$file,
+
+    ($e, $c) = $dom =~ /srces\.push\(\{type:\"video\/mp4\",src:\w+\('([^']+)',(\d+)/;
+    $file = $self->decode( $e, $c );
+    return 0 unless $file;
+
+    return $file = [{
+            url => 'http:'.$file,
             stream => 1,
         }];
-    } else {
-        return 0;
+}
+
+sub decode {
+    my( $self, $encoded, $code ) = @_;
+    my( @juice, %index, $t );
+    my ( $n, $r, $i, $s, $o, $u, $a ) = (0)x7;
+
+    $t = "";
+
+    # set up the array
+    @juice = ( 'A'..'Z', 'a'..'z', 0..9,'+','/','=' );
+    @juice = reverse( @juice );
+
+    # Create an index
+    @index{@juice} = (0..$#juice);
+
+    # check
+    return 0 unless ( (length($encoded) % 4) == 0 );
+    # Decode
+    foreach my $word ( $encoded =~ /(....)/g ) {
+        ( $s, $o, $u, $a ) = map { $index{ $_ } } split //, $word;
+        
+        $n = $s << 2 | $o >> 4;
+        $r = ( 15 & $o ) << 4 | $u >> 2;
+        $i = ( 3 & $u ) << 6 | $a;
+        $n = $n ^ $code;
+
+        $t .= chr($n);
+        $t .= chr($r) if 64 != $u;
+        $t .= chr($i) if 64 != $a;
     }
+
+    return $t;
 }
 
 1;
